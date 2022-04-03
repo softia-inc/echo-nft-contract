1cffba67f1353904c3bc6317870dfad40cdbf1b2
tkyatg <takuya.engineer911@gmail.com>
2022-04-03 06:14:31 +0000

    test

--- test/echoNft.js
+++ test/echoNft.js
@@ -1,18 +1,15 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
-describe('echoNft', function () {
-  it("Should return the new greeting once it's changed", async function () {
+describe('testing echoNft contract', function () {
+  it('mint successed', async function () {
     const echoNftContract = await ethers.getContractFactory('echoNFT');
     const contract = await echoNftContract.deploy();
     await contract.deployed();
-    // expect(await greeter.greet()).to.equal('Hello, world!');
 
-    // const setGreetingTx = await greeter.setGreeting('Hola, mundo!');
+    const mint = await contract.mintNFT(1);
+    await mint.wait();
 
-    // // wait until the transaction is mined
-    // await setGreetingTx.wait();
-
-    // expect(await greeter.greet()).to.equal('Hola, mundo!');
+    expect(contract.minted[1]).to.equal(true);
   });
 });
