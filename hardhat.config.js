8200b088f69b59974e9b4e8a9b7dc7c88aed3339
acall <acall@takuyanoMacBook-Pro.local>
2022-06-04 14:35:05 +0900

    env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
 require("@openzeppelin/hardhat-upgrades");
-
 module.exports = {
   solidity: "0.8.4",
   networks: {
     ropsten: {
       url: "https://ropsten.infura.io/v3/f3817857cc6e4679907a60270c5d1763",
-      accounts: [""],
+      accounts: [process.env.PRIVATE_KEY],
     },
     matic: {
       url: "https://polygon-rpc.com",
-      accounts: [""],
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
